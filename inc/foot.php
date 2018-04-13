16315733b22b23b17f29c8882a52e12a5fa16aa0
Lassi Heikkinen <lassi@pulu.org>
2018-04-13 19:32:03 +0000

    Fix footer

--- inc/foot.php
+++ inc/foot.php
@@ -4,7 +4,7 @@
 
 </div>
 
-<div class="copyright"><a href="https://lassi.pulu.org/">Lassi Heikkinen</a>, 2007-<? echo date(Y); ?></div>
+<div class="copyright"><a href="https://lassi.pulu.org/">Lassi Heikkinen</a>, 2007-<?php echo date('Y'); ?></div>
 
 </body>
 
